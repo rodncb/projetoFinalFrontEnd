f7977968f701f1e9cffcfff2f5fd16958cd1b651
rodncb <rodncb@gmail.com>
2025-06-05 14:04:10 -0300

    Update TMDB API key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // API Configuration - TMDB (The Movie Database)
-export const API_KEY = "SUA_CHAVE_TMDB_AQUI"; // Substitua pela sua chave TMDB
+export const API_KEY = "ad31f34950f8a7f28b7ec409210d5834";
 export const BASE_URL = "https://api.themoviedb.org/3";
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"; // Para posters dos filmes
 
